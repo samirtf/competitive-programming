7ba651cea69916257d3f9409a781aae200687cb2
samirtf <samircc20092@gmail.com>
2020-10-31 19:12:49 -0300

    feature: optimize space complexity in k-th function

--- cs/python/algorithms/src/dp/staircase/test_StaircaseWithUpKSteps.py
+++ cs/python/algorithms/src/dp/staircase/test_StaircaseWithUpKSteps.py
@@ -5,4 +5,7 @@ from src.dp.staircase.StaircaseWithUpKSteps import StaircaseWithUpKSteps
 
 class TestStaircaseWithUpKSteps(TestCase):
     def test_climb_stairs_with_up_3_steps(self):
-        self.assertEqual(StaircaseWithUpKSteps.climb_stairs_with_up_3_steps(8, 3), 81)
+        self.assertEqual(81, StaircaseWithUpKSteps.climb_stairs_with_up_3_steps(8, 3))
+
+    def test_climb_stairs_with_up_3_steps_optimized(self):
+        self.assertEqual(81, StaircaseWithUpKSteps.climb_stairs_with_up_3_steps_optimized(8, 3))
